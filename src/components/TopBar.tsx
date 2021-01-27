4e64c26d92c8e2798553a4a068cab9482179d080
tiffany1618 <tiffany9342@gmail.com>
2021-01-26 17:58:10 -0800

    Fixed stuff

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, Form, FormControl, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {Nav, Navbar, NavDropdown} from "react-bootstrap";
 import links from "../res/links";
 import github_logo from "../res/images/github_logo.png";
 import linkedin_logo from "../res/images/linkedin_logo.png";
