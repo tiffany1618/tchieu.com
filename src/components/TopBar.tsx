ce30f872ab13be5ae628201cb12b0543bcdb1de3
tiffany1618 <tiffany9342@gmail.com>
2021-01-26 17:40:38 -0800

    Changed nav bar to dark theme

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -9,7 +9,7 @@ import resume from "../res/resume.pdf";
 class TopBar extends React.Component {
     render() {
         return (
-            <Navbar bg="light" variant="light" expand="lg">
+            <Navbar bg="dark" variant="dark" expand="lg">
                 <Navbar.Brand href="#home">Tiffany C. Chieu</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
